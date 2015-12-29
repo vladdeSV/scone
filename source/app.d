9818fca9534bd66b2404acb112e17be16881dcfe
Vladde Nordholm <v@vladde.net>
2015-12-29 14:24:39 +0100

    Add close to test app

--- source/app.d
+++ source/app.d
@@ -2,7 +2,6 @@ import scone.core;
 import std.random;
 import std.stdio;
 
-
 void main()
 {
     sconeInit(SconeModule.Window);
@@ -12,9 +11,11 @@ void main()
     layer.write(0, 0, fg.red, bg.white, "hello there kott and blubeeries, wat are yoy doing this beautyiur beuirituyr nightrevening?? i am stiitngi ghere hanad dtyryugin to progrmamam this game enrgniergn that is for the solnosle");
     layer.print();
 
-    while (true)
+    foreach(n; 0 .. 100)
     {
         layer.write(uniform(0, 40), uniform(0, 20), fg.red, bg.white, uniform(1, 100));
         layer.print();
     }
+
+    sconeClose();
 }
