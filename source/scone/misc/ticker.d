e8d790ff560e392406c593e9a0b409d4f7b1278e
Vladde Nordholm <v@vladde.net>
2016-10-22 20:22:35 +0200

    Add ticking module

---
+++ source/scone/misc/ticker.d
@@ -0,0 +1,58 @@
+module scone.misc.ticker;
+
+import std.conv : to;
+import core.time : MonoTime, Duration;
+
+/**
+ * A general ticker, intended for games. Used to count how many times the game should update.
+ *
+ * Example:
+ * ---
+ * auto ticker = Ticker(60); // update 60 times per second
+ * foreach(tick; 0 .. ticker.ticks) // `ticker.ticks` returns how many time to ticks since last call
+ * {
+ *     // update logic, and only be run 60 times per second
+ * }
+ * ---
+ */
+struct Ticker
+{
+    /**
+     * Set custom ticks per second.
+     */
+    this(uint ticksPerSecond)
+    {
+        _interval = 1000.0/double(ticksPerSecond);
+        reset();
+    }
+
+    /**
+     * Returns: int, how many ticks that should be run since last call.
+     */
+    auto ticks() @property
+    {
+        /* Kudos to Yepoleb who helped me with this */
+        MonoTime newtime = MonoTime.currTime();
+        Duration duration = newtime - _lasttime;
+        long durationmsec = duration.total!"msecs";
+
+        _lasttime = newtime;
+        _msecs += durationmsec;
+        int ticks = to!int(_msecs / _interval);
+        _msecs -= to!int(ticks * _interval);
+
+        return ticks;
+    }
+
+    /**
+     * Resets the ticks since last call `ticks()`
+     */
+    void reset()
+    {
+        _lasttime = MonoTime.currTime();
+    }
+
+    private MonoTime _lasttime;
+    private double _interval;
+    private long _msecs = 60;
+}
