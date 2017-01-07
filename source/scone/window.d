d20d7e32e460d239276f6e83cd73ecfb88f922a3
Vladde Nordholm <v@vladde.net>
2017-01-07 10:36:12 +0100

    updated window

--- source/scone/window.d
+++ source/scone/window.d
@@ -4,9 +4,6 @@ struct Window
 {
     this(uint width, uint height)
     {
-        _width = width;
-        _height = height;
-
         _cells = new Cell[][](width, height);
         _backbuffer = _cells;
 
@@ -16,6 +13,8 @@ struct Window
     //TODO
     auto print()
     {
+        clear();
+        
         version(Windows)
         {
             foreach(cy, ref row; _cells)
@@ -24,7 +23,7 @@ struct Window
                 {
                     if(cell != _backbuffer[cy][cx])
                     {
-                        //TODO: writeSlot(cx, cy, cell);
+                        //TODO: writecell(cx, cy, cell);
                         _backbuffer[cy][cx] = cell;
                     }
                 }
@@ -33,11 +32,77 @@ struct Window
 
         version(Posix)
         {
+            enum rowUnchanged = -1;
+            import std.conv : to, text;
 
+            //Temporary string that will be printed out for each line.
+            string printed;
+
+            //Loop through all rows.
+            foreach (sy, row; _cells)
+            {
+                //f = first modified cell, l = last modified cell
+                uint f = rowUnchanged, l;
+
+                //TOOD: add a foreach_reverse to maybe speed up time
+                //Go through each line
+                foreach(sx, cell; _cells[sy])
+                {
+                    //If the cell at current position differs from backbuffer
+                    if(cell != _backbuffer[sy][sx])
+                    {
+                        //Set f once
+                        if(f == rowUnchanged)
+                        {
+                            f = to!uint(sx);
+                        }
+
+                        //Update l as many times as needed
+                        l = to!uint(sx);
+
+                        //Backbuffer is checked, make it "un-differ"
+                        _backbuffer[sy][sx] = cell;
+                    }
+                }
+
+                //If no cell on this row has been modified, continue
+                if(f == rowUnchanged)
+                {
+                    continue;
+                }
+
+                //Loop from the first changed cell to the last edited cell.
+                foreach (px; f .. l + 1)
+                {
+                    //TODO: colors are not supported yet on POSIX
+                    printed ~= text("\033[", 0, ";", cast(int)(_cells[sy][px].foreground), ";", cast(int)(_cells[sy][px].background + 10), "m", _cells[sy][px].character, "\033[0m");
+                }
+
+                //Set the cursor at the firstly edited cell...
+                setCursor(f, to!uint(sy));
+                //...and then print out the string via the terminals write function.
+                std.stdio.write(printed);
+
+                //Reset 'printed'.
+                printed = null;
+            }
+
+            //Flush. Without this problems may occur.
+            //stdout.flush(); //TODO: Check if needed for POSIX. I know without this it caused a lot of problems on the Windows console, but you know... this part is POSIX only
+        }
+    }
+    
+    ///Causes next `print()` to write out all cells.
+    ///NOTE: Only use this if some sort of visual bug occurs.
+    auto flush()
+    {
+        foreach(ref row; _backbuffer)
+        {
+            row[] = Cell(' ');
         }
     }
 
-    auto clear()
+    private auto clear()
     {
         foreach(ref row; _cells)
         {
@@ -45,22 +110,6 @@ struct Window
         }
     }
 
-    alias width  = w;
-    alias height = h;
-
-    /** Get the width of the frame */
-    auto w() const @property
-    {
-        return _width;
-    }
-
-    /** Get the height of the frame */
-    auto h() const @property
-    {
-        return _height;
-    }
-
-    private uint _width, _height;
     private Cell[][] _cells, _backbuffer;
 }
 
