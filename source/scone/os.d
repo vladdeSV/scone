528063eeecf0e394f53613ae5de383dba9158f1b
Vladimirs Nordholm <v@vladde.net>
2017-02-23 18:50:40 +0100

    travis

--- source/scone/os.d
+++ source/scone/os.d
@@ -1272,7 +1272,7 @@ struct OS
             }
 
             //to see if it stops polling input
-            writeln(" "); //adding this caused travis to pass...
+            writeln("."); //adding this caused travis to pass...
         }
 
         //globally shared
