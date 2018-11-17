459798a9fd914a52f26a5887aec568061229a46b
Vladimirs Nordholm <v@vladde.net>
2018-11-17 01:25:48 +0100

    split up input.d

---
+++ source/scone/input/package.d
@@ -0,0 +1,4 @@
+module scone.input;
+
+public import scone.input.keys;
+public import scone.input.input_event;
--- source/scone/package.d
+++ source/scone/package.d
@@ -3,5 +3,4 @@ module scone;
 public import scone.color;
 public import scone.core;
 public import scone.input;
-public import scone.os;
 public import scone.window;
