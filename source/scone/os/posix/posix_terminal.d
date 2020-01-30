a45478196b313e06094f79298479c86994d6b804
Vladimirs Nordholm <v@vladde.net>
2020-01-30 17:04:57 +0100

    make posix terminal available only on posix

--- source/scone/os/posix/posix_terminal.d
+++ source/scone/os/posix/posix_terminal.d
@@ -1,31 +1,34 @@
 module scone.os.posix.posix_terminal;
 
-import scone.input.input_event : InputEvent;
-import scone.input.scone_key : SK;
-import scone.input.scone_control_key : SCK;
-import scone.os.os_window : OSWindow;
-import scone.window.buffer : Buffer;
-import scone.window.types.size : Size;
-
-class PosixTerminal : OSWindow
+version (Posix)
 {
-    this()
+    import scone.input.input_event : InputEvent;
+    import scone.input.scone_key : SK;
+    import scone.input.scone_control_key : SCK;
+    import scone.os.os_window : OSWindow;
+    import scone.window.buffer : Buffer;
+    import scone.window.types.size : Size;
+
+    class PosixTerminal : OSWindow
     {
+        this()
+        {
 
-    }
+        }
 
-    public void renderBuffer(Buffer buffer)
-    {
+        public void renderBuffer(Buffer buffer)
+        {
 
-    }
+        }
 
-    public Size windowSize()
-    {
-        return Size(80, 24);
-    }
+        public Size windowSize()
+        {
+            return Size(80, 24);
+        }
 
-    public InputEvent[] latestInputEvents()
-    {
-        return [InputEvent(SK.a, SCK.none, true)];
+        public InputEvent[] latestInputEvents()
+        {
+            return [InputEvent(SK.a, SCK.none, true)];
+        }
     }
 }
