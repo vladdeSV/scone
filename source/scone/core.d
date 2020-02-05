72bc3d68970f5d50c938f02cc90fc6d9b29a44b9
Vladimirs Nordholm <v@vladde.net>
2020-02-05 17:33:26 +0100

    WIP: add dummy window for unittests + debug output

--- source/scone/core.d
+++ source/scone/core.d
@@ -21,18 +21,28 @@ static this()
         return;
     }
 
-    version(Posix)
+    import std.stdio : writeln;
+
+    version(unittest)
+    {
+        import scone.misc.dummy_window : DummyWindow;
+
+        window = new DummyWindow();
+        writeln("dummy");
+    }
+    else version(Posix)
     {
         import scone.os.posix.posix_terminal : PosixTerminal;
 
         window = new PosixTerminal();
+        writeln("posix");
     }
-
-    version(Windows)
+    else version(Windows)
     {
         import scone.os.windows.windows_console : WindowsConsole;
 
         window = new WindowsConsole();
+        writeln("windows");
     }
 
     frame = new Frame(window);
