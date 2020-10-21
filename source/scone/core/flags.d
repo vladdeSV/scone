ab01a08cc19775e8eac45ee116a344bafad2600d
Vladimirs Nordholm <v@vladde.net>
2020-10-21 14:52:03 +0200

    add unittests for flags (part of #39)

--- source/scone/core/flags.d
+++ source/scone/core/flags.d
@@ -16,3 +16,15 @@ T withoutFlag(T)(T value, T flag) if (isIntegral!(T))
 {
     return value & ~flag;
 }
+
+unittest
+{
+    assert(0b1111.hasFlag(0b0001));
+    assert(!0b1110.hasFlag(0b0001));
+
+    assert(0b1000.withFlag(0b0001) == 0b1001);
+    assert(0b1001.withFlag(0b0001) == 0b1001);
+
+    assert(0b1111.withoutFlag(0b0001) == 0b1110);
+    assert(0b1110.withoutFlag(0b0001) == 0b1110);
+}
