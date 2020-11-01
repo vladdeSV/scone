f48e69cf330034c521ffae2eab5909c82554f59b
Vladimirs Nordholm <v@vladde.net>
2020-11-01 15:31:17 +0100

    add unittest for Input

--- source/scone/input/input.d
+++ source/scone/input/input.d
@@ -23,3 +23,22 @@ class Input
 
     private StandardInput input;
 }
+
+unittest
+{
+    import scone.core.dummy : DummyInput;
+    import scone.input.keyboard_event : KeyboardEvent;
+    import scone.input.scone_control_key : SCK;
+    import scone.input.scone_key : SK;
+    auto stdin = new DummyInput();
+
+    auto input = new Input(stdin);
+    assert(input.keyboard() == []);
+
+    stdin.appendDummyKeyboardInput(KeyboardEvent(SK.a, SCK.none, true));
+    stdin.appendDummyKeyboardInput(KeyboardEvent(SK.b, SCK.none, true));
+    assert(input.keyboard() == [KeyboardEvent(SK.a, SCK.none, true), KeyboardEvent(SK.b, SCK.none, true)]);
+    assert(input.keyboard() == []);
+
+    destroy(input);
+}
