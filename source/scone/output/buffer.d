c6c53140b0924dd37dd3b8ade7e3eda42acb7ebf
Vladimirs Nordholm <v@vladde.net>
2020-11-02 17:14:00 +0000

    rewrite buffer

--- source/scone/output/buffer.d
+++ source/scone/output/buffer.d
@@ -4,60 +4,124 @@ import scone.output.types.cell : Cell;
 import scone.output.types.color;
 import scone.output.types.coordinate : Coordinate;
 import scone.output.types.size : Size;
+import std.range : chunks;
 
 class Buffer
 {
+    deprecated
+    {
+        alias setCell = stage;
+        alias changedCellCoordinates = diffs;
+        alias cellAt = get;
+        alias redraw = flush;
+    }
+
     this(Size size)
     {
-
+        this.size = size;
     }
 
     Size size()
     {
-        return Size(80, 24);
+        return this.bufferSize;
     }
 
-    void setCell(Coordinate coordinate, Cell cell) // fixme: rename `stage`
+    void size(Size size)
     {
-
+        this.buffer = new Cell[](size.width * size.height);
+        this.staging = this.buffer.dup;
+        this.bufferSize = size;
     }
 
-    Coordinate[] changedCellCoordinates() // fixme: rename `diffs`
+    void stage(Coordinate coordinate, Cell cell)
     {
-        return [];
+        assert(coordinate.x < this.bufferSize.width);
+        assert(coordinate.y < this.bufferSize.height);
+        assert(coordinate.x >= 0);
+        assert(coordinate.y >= 0);
+
+        const Cell bufferCell = this.get(coordinate);
+
+        if (cell.foreground == Color.same)
+        {
+            cell.foreground = bufferCell.foreground;
+        }
+
+        if (cell.background == Color.same)
+        {
+            cell.background = bufferCell.background;
+        }
+
+        auto view = this.staging.chunks(this.bufferSize.width);
+        view[coordinate.y][coordinate.x] = cell;
     }
 
-    Cell cellAt(Coordinate coordinate) // fixme: rename `get`
+    Coordinate[] diffs()
     {
-        return Cell();
+        Coordinate[] coordinates;
+        foreach (i; 0 .. this.buffer.length)
+        {
+            if (this.buffer[i] == this.staging[i] && !this.willFlush)
+            {
+                continue;
+            }
+
+            size_t y = cast(size_t)(i / this.bufferSize.width);
+            size_t x = i % this.bufferSize.width;
+
+            coordinates ~= Coordinate(x, y);
+        }
+
+        this.willFlush = false;
+
+        return coordinates;
+    }
+
+    Cell get(Coordinate coordinate)
+    {
+        assert(coordinate.x < this.bufferSize.width);
+        assert(coordinate.y < this.bufferSize.height);
+        assert(coordinate.x >= 0);
+        assert(coordinate.y >= 0);
+
+        auto view = this.staging.chunks(this.bufferSize.width);
+        return view[coordinate.y][coordinate.x];
     }
 
     void commit()
     {
-
+        this.buffer = this.staging.dup;
     }
 
-    void redraw() // fixme: delete and move logic to `void size(Size size) { ... }`
+    void flush()
     {
+        this.willFlush = true;
     }
+
+    private Size bufferSize;
+    private Cell[] buffer, staging;
+
+    private bool willFlush = true;
+
+    invariant(buffer.length == staging.length);
 }
 
 unittest
 {
     auto buffer = new Buffer(Size(4, 2));
-    assert(buffer.changedCellCoordinates.length == 8);
+    assert(buffer.diffs.length == 8);
 
     buffer.commit();
-    assert(buffer.changedCellCoordinates.length == 0);
+    assert(buffer.diffs.length == 0);
 
-    assert(buffer.cellAt(Coordinate(0, 0)) == Cell());
-    buffer.setCell(Coordinate(0, 0), Cell('1'));
-    assert(buffer.cellAt(Coordinate(0, 0)) == Cell('1'));
-    assert(buffer.changedCellCoordinates == [Coordinate(0, 0)]);
+    assert(buffer.get(Coordinate(1, 1)) == Cell());
+    buffer.stage(Coordinate(1, 1), Cell('1'));
+    assert(buffer.get(Coordinate(1, 1)) == Cell('1'));
+    assert(buffer.diffs == [Coordinate(1, 1)]);
 
     buffer.commit();
-    assert(buffer.cellAt(Coordinate(0, 0)) == Cell('1'));
-    assert(buffer.changedCellCoordinates.length == 0);
+    assert(buffer.get(Coordinate(1, 1)) == Cell('1'));
+    assert(buffer.diffs.length == 0);
 }
 
 /+
