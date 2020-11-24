27b887bfeca6ba728173f8d3e4299afca845a3cc
Vladimirs Nordholm <v@vladde.net>
2020-11-24 11:30:34 +0000

    clarify ansi function name

--- source/scone/output/os/posix/ansi_color_helper.d
+++ source/scone/output/os/posix/ansi_color_helper.d
@@ -10,7 +10,7 @@ version (Posix)
         background,
     }
 
-    auto ansiNumber(AnsiColor ansi, AnsiColorType type)
+    auto ansiColorNumber(AnsiColor ansi, AnsiColorType type)
     {
         int number = 0;
 
@@ -81,48 +81,48 @@ version (Posix)
     ///
     unittest
     {
-        assert(ansiNumber(AnsiColor.initial, AnsiColorType.foreground) == 39);
+        assert(ansiColorNumber(AnsiColor.initial, AnsiColorType.foreground) == 39);
 
-        assert(ansiNumber(AnsiColor.blackDark, AnsiColorType.foreground) == 30);
-        assert(ansiNumber(AnsiColor.redDark, AnsiColorType.foreground) == 31);
-        assert(ansiNumber(AnsiColor.greenDark, AnsiColorType.foreground) == 32);
-        assert(ansiNumber(AnsiColor.yellowDark, AnsiColorType.foreground) == 33);
-        assert(ansiNumber(AnsiColor.blueDark, AnsiColorType.foreground) == 34);
-        assert(ansiNumber(AnsiColor.magentaDark, AnsiColorType.foreground) == 35);
-        assert(ansiNumber(AnsiColor.cyanDark, AnsiColorType.foreground) == 36);
-        assert(ansiNumber(AnsiColor.whiteDark, AnsiColorType.foreground) == 37);
+        assert(ansiColorNumber(AnsiColor.blackDark, AnsiColorType.foreground) == 30);
+        assert(ansiColorNumber(AnsiColor.redDark, AnsiColorType.foreground) == 31);
+        assert(ansiColorNumber(AnsiColor.greenDark, AnsiColorType.foreground) == 32);
+        assert(ansiColorNumber(AnsiColor.yellowDark, AnsiColorType.foreground) == 33);
+        assert(ansiColorNumber(AnsiColor.blueDark, AnsiColorType.foreground) == 34);
+        assert(ansiColorNumber(AnsiColor.magentaDark, AnsiColorType.foreground) == 35);
+        assert(ansiColorNumber(AnsiColor.cyanDark, AnsiColorType.foreground) == 36);
+        assert(ansiColorNumber(AnsiColor.whiteDark, AnsiColorType.foreground) == 37);
 
-        assert(ansiNumber(AnsiColor.black, AnsiColorType.foreground) == 90);
-        assert(ansiNumber(AnsiColor.red, AnsiColorType.foreground) == 91);
-        assert(ansiNumber(AnsiColor.green, AnsiColorType.foreground) == 92);
-        assert(ansiNumber(AnsiColor.yellow, AnsiColorType.foreground) == 93);
-        assert(ansiNumber(AnsiColor.blue, AnsiColorType.foreground) == 94);
-        assert(ansiNumber(AnsiColor.magenta, AnsiColorType.foreground) == 95);
-        assert(ansiNumber(AnsiColor.cyan, AnsiColorType.foreground) == 96);
-        assert(ansiNumber(AnsiColor.white, AnsiColorType.foreground) == 97);
+        assert(ansiColorNumber(AnsiColor.black, AnsiColorType.foreground) == 90);
+        assert(ansiColorNumber(AnsiColor.red, AnsiColorType.foreground) == 91);
+        assert(ansiColorNumber(AnsiColor.green, AnsiColorType.foreground) == 92);
+        assert(ansiColorNumber(AnsiColor.yellow, AnsiColorType.foreground) == 93);
+        assert(ansiColorNumber(AnsiColor.blue, AnsiColorType.foreground) == 94);
+        assert(ansiColorNumber(AnsiColor.magenta, AnsiColorType.foreground) == 95);
+        assert(ansiColorNumber(AnsiColor.cyan, AnsiColorType.foreground) == 96);
+        assert(ansiColorNumber(AnsiColor.white, AnsiColorType.foreground) == 97);
     }
     ///
     unittest
     {
-        assert(ansiNumber(AnsiColor.initial, AnsiColorType.background) == 49);
+        assert(ansiColorNumber(AnsiColor.initial, AnsiColorType.background) == 49);
 
-        assert(ansiNumber(AnsiColor.blackDark, AnsiColorType.background) == 40);
-        assert(ansiNumber(AnsiColor.redDark, AnsiColorType.background) == 41);
-        assert(ansiNumber(AnsiColor.greenDark, AnsiColorType.background) == 42);
-        assert(ansiNumber(AnsiColor.yellowDark, AnsiColorType.background) == 43);
-        assert(ansiNumber(AnsiColor.blueDark, AnsiColorType.background) == 44);
-        assert(ansiNumber(AnsiColor.magentaDark, AnsiColorType.background) == 45);
-        assert(ansiNumber(AnsiColor.cyanDark, AnsiColorType.background) == 46);
-        assert(ansiNumber(AnsiColor.whiteDark, AnsiColorType.background) == 47);
+        assert(ansiColorNumber(AnsiColor.blackDark, AnsiColorType.background) == 40);
+        assert(ansiColorNumber(AnsiColor.redDark, AnsiColorType.background) == 41);
+        assert(ansiColorNumber(AnsiColor.greenDark, AnsiColorType.background) == 42);
+        assert(ansiColorNumber(AnsiColor.yellowDark, AnsiColorType.background) == 43);
+        assert(ansiColorNumber(AnsiColor.blueDark, AnsiColorType.background) == 44);
+        assert(ansiColorNumber(AnsiColor.magentaDark, AnsiColorType.background) == 45);
+        assert(ansiColorNumber(AnsiColor.cyanDark, AnsiColorType.background) == 46);
+        assert(ansiColorNumber(AnsiColor.whiteDark, AnsiColorType.background) == 47);
 
-        assert(ansiNumber(AnsiColor.black, AnsiColorType.background) == 100);
-        assert(ansiNumber(AnsiColor.red, AnsiColorType.background) == 101);
-        assert(ansiNumber(AnsiColor.green, AnsiColorType.background) == 102);
-        assert(ansiNumber(AnsiColor.yellow, AnsiColorType.background) == 103);
-        assert(ansiNumber(AnsiColor.blue, AnsiColorType.background) == 104);
-        assert(ansiNumber(AnsiColor.magenta, AnsiColorType.background) == 105);
-        assert(ansiNumber(AnsiColor.cyan, AnsiColorType.background) == 106);
-        assert(ansiNumber(AnsiColor.white, AnsiColorType.background) == 107);
+        assert(ansiColorNumber(AnsiColor.black, AnsiColorType.background) == 100);
+        assert(ansiColorNumber(AnsiColor.red, AnsiColorType.background) == 101);
+        assert(ansiColorNumber(AnsiColor.green, AnsiColorType.background) == 102);
+        assert(ansiColorNumber(AnsiColor.yellow, AnsiColorType.background) == 103);
+        assert(ansiColorNumber(AnsiColor.blue, AnsiColorType.background) == 104);
+        assert(ansiColorNumber(AnsiColor.magenta, AnsiColorType.background) == 105);
+        assert(ansiColorNumber(AnsiColor.cyan, AnsiColorType.background) == 106);
+        assert(ansiColorNumber(AnsiColor.white, AnsiColorType.background) == 107);
     }
 
     string ansiColorString(Color foreground, Color background)
@@ -132,15 +132,15 @@ version (Posix)
 
         if (foreground.state == ColorState.ansi && background.state == ColorState.ansi)
         {
-            auto foregroundNumber = ansiNumber(foreground.ansi, AnsiColorType.foreground);
-            auto backgroundNumber = ansiNumber(background.ansi, AnsiColorType.background);
+            auto foregroundNumber = ansiColorNumber(foreground.ansi, AnsiColorType.foreground);
+            auto backgroundNumber = ansiColorNumber(background.ansi, AnsiColorType.background);
             return text("\033[0;", foregroundNumber, ";", backgroundNumber, "m",);
         }
 
         string ret;
         if (foreground.state == ColorState.ansi)
         {
-            ret ~= text("\033[", ansiNumber(foreground.ansi, AnsiColorType.foreground), "m");
+            ret ~= text("\033[", ansiColorNumber(foreground.ansi, AnsiColorType.foreground), "m");
         }
         else if (foreground.state == ColorState.rgb)
         {
@@ -149,7 +149,7 @@ version (Posix)
 
         if (background.state == ColorState.ansi)
         {
-            ret ~= text("\033[", ansiNumber(background.ansi, AnsiColorType.background), "m");
+            ret ~= text("\033[", ansiColorNumber(background.ansi, AnsiColorType.background), "m");
         }
         else if (background.state == ColorState.rgb)
         {
